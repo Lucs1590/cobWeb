4397916d468b9e42770952c9dd48c948524730a5
Lucas Brito <lucasbsilva29@gmail.com>
2025-08-20 22:57:46 -0300

    refactor: enhance MongoDB integration and logging for plant data processing

--- PlantDan.py
+++ PlantDan.py
@@ -1,184 +1,231 @@
 from lxml import html
 import requests
 import pymongo
+from typing import Dict, Optional
+import logging
 
-####################################################### DECLARACAO DE VARIAVEIS #######################################################################
-Linha = 977
-a = 0
-cont = 2
-regiaosite = []
-nome_cien_site = []
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-div = []
-cic = []
-rep = []
-habit = []
-adap = []
-alt = []
 
-fil = []
-for_limb = []
-sup = []
-consist = []
-nerv = []
-comp = []
+def main():
+    # Get user input
+    plant_name = input(
+        'Digite o nome cientifico da planta-daninha\n'
+    ).lower().capitalize()
 
-inf = []
-peri = []
+    char_mapping = create_character_mapping()
+    normalized_plant_name = normalize_plant_name(plant_name, char_mapping)
 
-tip_fruto = []
-obs = []
-especiais01 = [['\\xc0', 'À'], ['\\xc1', 'Á'],	['\\xc2', 'Â'],	['\\xc3', 'Ã'],	['\\xc4', 'Ä'],	['\\xc7', 'Ç'],	['\\xc8', 'È'],	['\\xc9', 'É'],	['\\xca', 'Ê'],	['\\xcb', 'Ë'],	['\\xcc', 'Ì'],	['\\xcd', 'Í'],	['\\xce', 'Î'],	['\\xcf', 'Ï'],	['\\xd1', 'Ñ'],	['\\xd2', 'Ò'],	['\\xd3', 'Ó'],	['\\xd4', 'Ô'],	['\\xd5', 'Õ'],	['\\xd6', 'Ö'],	['\\xd9', 'Ù'],	['\\xda', 'Ú'],	['\\xdb', 'Û'],	[
-    '\\xdc', 'Ü'],	['\xe0', 'à'],	['\\xe2', 'â'],	['\\xe3', 'ã'],	['\xe4', 'ä'],	['\\xe7', 'ç'],	['\\xe8', 'è'],	['\\xe9', 'é'],	['\\xea', 'ê'],	['\\xeb', 'ë'],	['\\xec', 'ì'],	['\\xed', 'í'],	['\\xee', 'î'],	['\\xef', 'ï'],	['\\xf1', 'ñ'],	['\\xf2', 'ò'],	['\\xf3', 'ó'],	['\\xf4', 'ô'],	['\\xf5', 'õ'],	['\\xf6', 'ö'],	['\\xf9', 'ù'],	['\\xfa', 'ú'],	['\\xfb', 'û'],	['\\xfc', 'ü'],	['\\xfd', 'ÿ']]
-####################################################### DESENVOLVIMENTO ###############################################################################
-# ---------------------------------------------------INTERACAO DO USUARIO-------------------------------------------------------------------------------
-nome_cien = input(
-    'Digite o nome cientifico da planta-daninha\n').lower().capitalize()
-n_nome_cient = str(nome_cien)
-# Cnidoscolus urens
-# -------------------------------------------------CONDICAO DOENCA OU INSETO----------------------------------------------------------------------------
-for x in especiais01:
-    if x[1] in nome_cien:
-        n_nome_cient = str.replace(n_nome_cient, x[1], str(x[0]))
+    try:
+        client = pymongo.MongoClient("localhost", 27017)
+        db = client["MongoDB_Samuel_01"]
+        logger.info("Connected to MongoDB")
+    except Exception as e:
+        logger.error("Error connecting to MongoDB: %s", e)
+        return
 
-conexao = pymongo.MongoClient("localhost", 27017)
-db = conexao["MongoDB_Samuel_01"]
+    output_filename = f'PRAGAS COBWEB - Planta-Daninha({plant_name}).csv'
 
-Novo = open('PRAGAS COBWEB - Planta-Daninha(%s).csv' % (nome_cien), 'w')
-while Linha <= 1400:
-    page = requests.get(str(
-        'http://agrofit.agricultura.gov.br/agrofit_cons/!ap_planta_detalhe_cons?p_id_planta_daninha=%d' % Linha))
-    tree = html.fromstring(page.content)
-    nome_cien_site = str(tree.xpath('id("N1")/table[1]/tr/td[2]/input/@value'))
-    # print str(nome_cien_site)
-    if str(nome_cien) in "*"+str(nome_cien_site)+"*":
-        div = tree.xpath(
-            'id("N2")/table[1]/tr[3]/td/table/tr/td/table/tr[2]/td[2]/input/@value')
-        cic = tree.xpath(
-            'id("N2")/table[1]/tr[3]/td/table/tr/td/table/tr[3]/td[2]/input/@value')
-        rep = tree.xpath(
-            'id("N2")/table[1]/tr[3]/td/table/tr/td/table/tr[4]/td[2]/input/@value')
-        habit = tree.xpath(
-            'id("N2")/table[1]/tr[3]/td/table/tr/td/table/tr[5]/td[2]/input/@value')
-        adap = tree.xpath(
-            'id("N2")/table[1]/tr[3]/td/table/tr/td/table/tr[6]/td[2]/input/@value')
-        alt = tree.xpath(
-            'id("N2")/table[1]/tr[3]/td/table/tr/td/table/tr[7]/td[2]/input/@value')
+    start_line = 977
+    end_line = 1400
 
-        fil = tree.xpath(
-            'id("N2")/table[1]/tr[4]/td/table/tr/td/table/tr[2]/td[2]/input/@value')
-        for_limb = tree.xpath(
-            'id("N2")/table[1]/tr[4]/td/table/tr/td/table/tr[3]/td[2]/input/@value')
-        sup = tree.xpath(
-            'id("N2")/table[1]/tr[4]/td/table/tr/td/table/tr[4]/td[2]/input/@value')
-        consist = tree.xpath(
-            'id("N2")/table[1]/tr[4]/td/table/tr/td/table/tr[5]/td[2]/input/@value')
-        nerv = tree.xpath(
-            'id("N2")/table[1]/tr[4]/td/table/tr/td/table/tr[6]/td[2]/input/@value')
-        comp = tree.xpath(
-            'id("N2")/table[1]/tr[4]/td/table/tr/td/table/tr[7]/td[2]/input/@value')
+    for linha in range(start_line, end_line + 1):
+        tree = fetch_plant_page(linha)
+        if not tree:
+            continue
 
-        inf = tree.xpath(
-            'id("N2")/table[1]/tr[5]/td/table/tr/td/table/tr[2]/td[2]/input/@value')
+        try:
+            site_plant_name = tree.xpath(
+                'id("N1")/table[1]/tr/td[2]/input/@value'
+            )
+            if not site_plant_name:
+                continue
 
-        tip_fruto = tree.xpath(
-            'id("N2")/table[1]/tr[6]/td/table/tr/td/table/tr[2]/td[2]/input/@value')
-        obs = tree.xpath(
-            'id("N2")/table[2]/tr/td/table/tr[1]/td/textarea/text()')
-# --------------------------------------------------------TRATAMENTO DO XPATH------------------------------------------------------------------------
-        if (len(str(div)) > 2) and (len(str(cic)) > 2) and (len(str(rep)) > 2) and (len(str(habit)) > 2) and (len(str(adap)) > 2) and (len(str(alt)) > 2) and (len(str(fil)) > 2) and (len(str(for_limb)) > 2) and (len(str(sup)) > 2) and (len(str(consist)) > 2) and (len(str(nerv)) > 2) and (len(str(comp)) > 2) and (len(str(inf)) > 2) and (len(str(tip_fruto)) > 2) and (len(str(obs)) > 2):
-            div = str(div[0].strip('\n\t').encode(
-                'utf-8')) if div != [] else ""
-            if div == '[]':
-                div == 'NÃO REGISTRADO'
-            cic = str(cic[0].strip('\n\t').encode(
-                'utf-8')) if cic != [] else ""
-            if cic == '[]':
-                cic = 'NÃO REGISTRADO'
-            rep = str(rep[0].strip('\n\t').encode(
-                'utf-8')) if rep != [] else ""
-            if rep == '[]':
-                rep = 'NÃO REGISTRADO'
-            habit = str(habit[0].strip('\n\t').encode(
-                'utf-8')) if habit != [] else ""
-# if habit == '[]':
-#	habit ='NÃO REGISTRADO'
-            adap = str(adap[0].strip('\n\t').encode(
-                'utf-8')) if adap != [] else ""
-            if adap == '[]':
-                adap = 'NÃO REGISTRADO'
-            alt = str(alt[0].strip('\n\t').encode(
-                'utf-8')) if alt != [] else ""
-            if alt == '[]':
-                alt = 'NÃO REGISTRADO'
-            fil = str(fil[0].strip('\n\t').encode(
-                'utf-8')) if fil != [] else ""
-            if fil == '[]':
-                fil = 'NÃO REGISTRADO'
-            for_limb = str(for_limb[0].strip('\n\t').encode(
-                'utf-8')) if for_limb != [] else ""
-            if for_limb == '[]':
-                for_limb = 'NÃO REGISTRADO'
-            sup = str(sup[0].strip('\n\t').encode(
-                'utf-8')) if sup != [] else ""
-            if sup == '[]':
-                sup = 'NÃO REGISTRADO'
-            consist = str(consist[0].strip('\n\t').encode(
-                'utf-8')) if consist != [] else ""
-            if consist == '[]':
-                consist = 'NÃO REGISTRADO'
-            nerv = str(nerv[0].strip('\n\t').encode(
-                'utf-8')) if nerv != [] else ""
-            if nerv == '[]':
-                nerv = 'NÃO REGISTRADO'
-            comp = str(comp[0].strip('\n\t').encode(
-                'utf-8')) if comp != [] else ""
-            if comp == '[]':
-                comp = 'NÃO REGISTRADO'
-            inf = str(inf[0].strip('\n\t').encode(
-                'utf-8')) if inf != [] else ""
-            if inf == '[]':
-                inf = 'NÃO REGISTRADO'
-            tip_fruto = str(tip_fruto[0].strip('\n\t').encode(
-                'utf-8')) if tip_fruto != [] else ""
-            if tip_fruto == '[]':
-                tip_fruto = 'NÃO REGISTRADO'
-            obs = str(obs[0].strip('\n\t').encode(
-                'utf-8')) if obs != [] else ""
-            if obs == '[]':
-                obs = 'NÃO REGISTRADO'
-# -------------------------------------------------------- ESTRUTURA E IMPRESSÃO------------------------------------------------------------------------
-        carac_plant = str('INFORMAÇÕES DA PLANTA\n\nDivisão: ' + str(div) + '\nCiclo: ' + str(cic) + '\nPropagação: ' + str(
-            rep) + '\nHabitat: ' + str(habit) + '\nAdaptação: ' + str(adap) + '\nAltura(cm): ' + str(alt) + "\n")  # .decode('utf-8')
-        carac_folha = str('\nINFORMAÇÕES DA FOLHA\n\nFilotaxia: ' + str(fil) + '\nFormato do Limbo: ' + str(for_limb) + '\nSuperfície: ' + str(
-            sup) + '\nConsistencia: ' + str(consist) + '\nNervação: ' + str(nerv) + '\nComprimeto: ' + str(comp) + "\n")  # .decode('utf-8')
-        floruto = str('\nFLOR, FRUTO E OBSERVAÇÃO\nInflorecência: ' + str(inf) + ' | ' +
-                      'Tipo do Fruto: ' + str(tip_fruto) + '\nObrservação: ' + str(obs))  # .decode('utf-8')
-        esp = '\n_________________________________________________________________________________________________________________________________________'
-        tudo = str(carac_plant) + str(carac_folha) + str(floruto) + str(esp)
+            site_plant_name = str(site_plant_name[0])
 
-        collection = (db.plant_dan.update(
-            {"linha": str(Linha),
-             "divisao": str(div)},
-            {"divisao": str(div),
-             "ciclo": str(cic),
-             "propagacao": str(rep),
-             "habitat": str(habit),
-             "adaptacao": str(adap),
-             "altura(cm)": str(alt),
-             "filotaxia": str(fil),
-             "form_limbo": str(for_limb),
-             "superficie": str(sup),
-             "consistencia": str(consist),
-             "nervacao": str(nerv),
-             "comprimento": str(comp),
-             "inflorecencia": str(inf),
-             "tip_fruto": str(tip_fruto),
-             "observacao": str(obs)}, upsert=True))
+            if plant_name in site_plant_name:
+                logger.info(
+                    "Found matching plant at line %d: %s",
+                    linha,
+                    site_plant_name
+                )
 
-        print(str(tudo))
-        Novo.write(str(tudo))
-        break
+                plant_data = extract_plant_data(tree)
+
+                non_empty_fields = [
+                    v for v in plant_data.values() if v != "NÃO REGISTRADO"
+                ]
+                if len(non_empty_fields) < 3:
+                    logger.warning(
+                        "Insufficient data found for line %d",
+                        linha
+                    )
+                    continue
+
+                formatted_info = format_plant_info(plant_data)
+
+                try:
+                    with open(output_filename, 'w', encoding='utf-8') as file:
+                        file.write(formatted_info)
+                    logger.info("Data saved to file: %s", output_filename)
+                except Exception as e:
+                    logger.error("Error writing to file: %s", e)
+
+                save_to_mongodb(db, plant_data, linha)
+                print(formatted_info)
+                break
+
+        except Exception as e:
+            logger.error("Error processing line %d: %s", linha, e)
+            continue
+
     else:
-        Linha += 1
-Novo.close()
+        logger.info("Plant '%s' not found in the specified range", plant_name)
+        print(
+            "Planta '%s' não foi encontrada no intervalo especificado.", plant_name
+        )
+
+    client.close()
+
+
+def create_character_mapping() -> Dict[str, str]:
+    """Create character encoding mapping for special characters."""
+    return {
+        'À': '\\xc0', 'Á': '\\xc1', 'Â': '\\xc2', 'Ã': '\\xc3', 'Ä': '\\xc4',
+        'Ç': '\\xc7', 'È': '\\xc8', 'É': '\\xc9', 'Ê': '\\xca', 'Ë': '\\xcb',
+        'Ì': '\\xcc', 'Í': '\\xcd', 'Î': '\\xce', 'Ï': '\\xcf', 'Ñ': '\\xd1',
+        'Ò': '\\xd2', 'Ó': '\\xd3', 'Ô': '\\xd4', 'Õ': '\\xd5', 'Ö': '\\xd6',
+        'Ù': '\\xd9', 'Ú': '\\xda', 'Û': '\\xdb', 'Ü': '\\xdc', 'à': '\xe0',
+        'â': '\\xe2', 'ã': '\\xe3', 'ä': '\xe4', 'ç': '\\xe7', 'è': '\\xe8',
+        'é': '\\xe9', 'ê': '\\xea', 'ë': '\\xeb', 'ì': '\\xec', 'í': '\\xed',
+        'î': '\\xee', 'ï': '\\xef', 'ñ': '\\xf1', 'ò': '\\xf2', 'ó': '\\xf3',
+        'ô': '\\xf4', 'õ': '\\xf5', 'ö': '\\xf6', 'ù': '\\xf9', 'ú': '\\xfa',
+        'û': '\\xfb', 'ü': '\\xfc', 'ÿ': '\\xfd'
+    }
+
+
+def normalize_plant_name(plant_name: str, char_mapping: Dict[str, str]) -> str:
+    """Normalize plant name by replacing special characters."""
+    normalized_name = plant_name
+    for original_char, encoded_char in char_mapping.items():
+        if original_char in normalized_name:
+            normalized_name = normalized_name.replace(
+                original_char,
+                encoded_char
+            )
+    return normalized_name
+
+
+def fetch_plant_page(linha: int) -> Optional[html.HtmlElement]:
+    """Fetch and parse plant page from the website."""
+    url = f'http://agrofit.agricultura.gov.br/agrofit_cons/!ap_planta_detalhe_cons?p_id_planta_daninha={linha}'
+
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+        return html.fromstring(response.content)
+    except requests.exceptions.RequestException as e:
+        logger.warning("Error fetching page for line %d: %s", linha, e)
+        return None
+
+
+def extract_plant_data(tree) -> Dict[str, str]:
+    """Extract all plant data from the parsed HTML tree."""
+    xpath_mappings = {
+        'divisao': 'id("N2")/table[1]/tr[3]/td/table/tr/td/table/tr[2]/td[2]/input/@value',
+        'ciclo': 'id("N2")/table[1]/tr[3]/td/table/tr/td/table/tr[3]/td[2]/input/@value',
+        'propagacao': 'id("N2")/table[1]/tr[3]/td/table/tr/td/table/tr[4]/td[2]/input/@value',
+        'habitat': 'id("N2")/table[1]/tr[3]/td/table/tr/td/table/tr[5]/td[2]/input/@value',
+        'adaptacao': 'id("N2")/table[1]/tr[3]/td/table/tr/td/table/tr[6]/td[2]/input/@value',
+        'altura': 'id("N2")/table[1]/tr[3]/td/table/tr/td/table/tr[7]/td[2]/input/@value',
+        'filotaxia': 'id("N2")/table[1]/tr[4]/td/table/tr/td/table/tr[2]/td[2]/input/@value',
+        'form_limbo': 'id("N2")/table[1]/tr[4]/td/table/tr/td/table/tr[3]/td[2]/input/@value',
+        'superficie': 'id("N2")/table[1]/tr[4]/td/table/tr/td/table/tr[4]/td[2]/input/@value',
+        'consistencia': 'id("N2")/table[1]/tr[4]/td/table/tr/td/table/tr[5]/td[2]/input/@value',
+        'nervacao': 'id("N2")/table[1]/tr[4]/td/table/tr/td/table/tr[6]/td[2]/input/@value',
+        'comprimento': 'id("N2")/table[1]/tr[4]/td/table/tr/td/table/tr[7]/td[2]/input/@value',
+        'inflorecencia': 'id("N2")/table[1]/tr[5]/td/table/tr/td/table/tr[2]/td[2]/input/@value',
+        'tip_fruto': 'id("N2")/table[1]/tr[6]/td/table/tr/td/table/tr[2]/td[2]/input/@value',
+        'observacao': 'id("N2")/table[2]/tr/td/table/tr[1]/td/textarea/text()'
+    }
+
+    return {field: extract_field_value(tree, xpath) for field, xpath in xpath_mappings.items()}
+
+
+def extract_field_value(tree, xpath: str) -> str:
+    """Extract and clean field value from XPath."""
+    try:
+        result = tree.xpath(xpath)
+        if result and len(result) > 0:
+            value = str(result[0]).strip('\n\t')
+            return value if value else "NÃO REGISTRADO"
+        return "NÃO REGISTRADO"
+    except Exception as e:
+        logger.warning("Error extracting field with XPath %s: %s", xpath, e)
+        return "NÃO REGISTRADO"
+
+
+def format_plant_info(data: Dict[str, str]) -> str:
+    """Format plant data into readable text."""
+    plant_info = f"""INFORMAÇÕES DA PLANTA
+
+Divisão: {data['divisao']}
+Ciclo: {data['ciclo']}
+Propagação: {data['propagacao']}
+Habitat: {data['habitat']}
+Adaptação: {data['adaptacao']}
+Altura(cm): {data['altura']}
+
+INFORMAÇÕES DA FOLHA
+
+Filotaxia: {data['filotaxia']}
+Formato do Limbo: {data['form_limbo']}
+Superfície: {data['superficie']}
+Consistencia: {data['consistencia']}
+Nervação: {data['nervacao']}
+Comprimeto: {data['comprimento']}
+
+FLOR, FRUTO E OBSERVAÇÃO
+Inflorecência: {data['inflorecencia']} | Tipo do Fruto: {data['tip_fruto']}
+Obrservação: {data['observacao']}
+_________________________________________________________________________________________________________________________________________"""
+
+    return plant_info
+
+
+def save_to_mongodb(db, data: Dict[str, str], linha: int) -> None:
+    """Save plant data to MongoDB."""
+    try:
+        document = {
+            "linha": str(linha),
+            "divisao": data['divisao'],
+            "ciclo": data['ciclo'],
+            "propagacao": data['propagacao'],
+            "habitat": data['habitat'],
+            "adaptacao": data['adaptacao'],
+            "altura(cm)": data['altura'],
+            "filotaxia": data['filotaxia'],
+            "form_limbo": data['form_limbo'],
+            "superficie": data['superficie'],
+            "consistencia": data['consistencia'],
+            "nervacao": data['nervacao'],
+            "comprimento": data['comprimento'],
+            "inflorecencia": data['inflorecencia'],
+            "tip_fruto": data['tip_fruto'],
+            "observacao": data['observacao']
+        }
+
+        db.plant_dan.update_one(
+            {"linha": str(linha)},
+            {"$set": document},
+            upsert=True
+        )
+        logger.info("Data saved to MongoDB for line %d", linha)
+    except Exception as e:
+        logger.error("Error saving to MongoDB: %s", e)
+
+
+if __name__ == "__main__":
+    main()
